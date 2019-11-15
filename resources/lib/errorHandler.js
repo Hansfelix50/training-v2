832dc310d83a05b5169622ac0c3720dae054e3a8
Hans Felix <hansfelix50@gmail.com>
2019-11-15 09:55:06 -0500

    errorHandler.js

---
+++ resources/lib/errorHandler.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const logger = require('../lib/logger');
+
+function processError(fn) {
+    return function (req, res, next) {
+        fn(req, res, next).catch(next);
+    }
+}
+// new CustomProductError
+function processDBError(err, req, res, next) {
+    if (err instanceof mongoose.Error || err.name === 'MongoError') {
+        logger.error('Ocurrió un error en la BD', err);
+        err.message = 'Error en la BD';
+        err.status = 500;
+    }
+    next(err);
+}
+
+function catchResolver(err, req, res, next) {
+    console.log(err);
+    res.status(err.status).send(err.message);
+}
+
+module.exports = {
+    processError,
+    processDBError,
+    catchResolver,
+}
